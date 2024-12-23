998dc37e32266890ba5de7a61f556f3fe4e5dbaa
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-23 09:03:42 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      <div class="app">
+        <NavBar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
