5f92280759611d14e057d0d9c8753f02747e297a
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-23 14:38:41 +0530

    add-header-files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import NavBar from './Components/NavBar/NavBar'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Pages/Home/Home';
+import Card from './Pages/Card/Card';
+import PlaceHolder from './Pages/PlaceOrder/PlaceHolder';
 
 const App = () => {
   return (
     <div>
-      <div class="app">
-        <NavBar/>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/card" element={<Card />} />
+          <Route path="/order" element={<PlaceHolder />} />
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Welcome to [App Name], your ultimate food ordering app designed to bring delicious meals right to your doorstep. Whether you're craving your favorite comfort food, exploring new cuisines, or looking for quick bites on the go, we've got you covered</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
