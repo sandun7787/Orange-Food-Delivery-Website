f177c31afb3d2b129eb9939f16c76057ef07ac70
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-23 10:06:01 +0530

    update-the-nev-bar

--- Frontend/src/Components/NavBar/NavBar.jsx
+++ Frontend/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
+import {assets} from '../../assets/assets'
 
 const NavBar = () => {
+  const[menu,setMenu]=useState("home");
   return (
     <div className='navbar'>
-      
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li className={menu === "home"? "active":""}>Home</li>
+        <li className={menu ==="Menu"?"active":""}>Menu</li>
+        <li className={menu ==="Mobile App"? "active":""}>Mobile-App</li>
+        <li className={menu ==="Contact-us"? "active":""}>Contatc-us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className='dot'></div>
+        </div>
+        <button>Sign in
+        </button>
+      </div>
     </div>
   )
 }
