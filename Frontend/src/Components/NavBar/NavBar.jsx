410495ac9e293da6a37818a9df4b794e61724ac9
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-23 12:51:44 +0530

    update-the-nav-bar

--- Frontend/src/Components/NavBar/NavBar.jsx
+++ Frontend/src/Components/NavBar/NavBar.jsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react'
-import './NavBar.css'
-import {assets} from '../../assets/assets'
+import React, { useState } from 'react';
+import './NavBar.css';
+import { assets } from '../../assets/assets';
 
 const NavBar = () => {
-  const[menu,setMenu]=useState("home");
+  const [menu, setMenu] = useState("home");
   return (
-    <div className='navbar'>
-      <img src={assets.logo} alt="" className="logo" />
+    <div className="navbar">
+      <img src={assets.logo} alt="Logo" className="logo" />
       <ul className="navbar-menu">
-        <li className={menu === "home"? "active":""}>Home</li>
-        <li className={menu ==="Menu"?"active":""}>Menu</li>
-        <li className={menu ==="Mobile App"? "active":""}>Mobile-App</li>
-        <li className={menu ==="Contact-us"? "active":""}>Contatc-us</li>
+        <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+        <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+        <li onClick={() => setMenu("mobile app")} className={menu === "mobile app" ? "active" : ""}>Mobile-App</li>
+        <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact-us</li>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
+        <img src={assets.search_icon} alt="Search Icon" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className='dot'></div>
+          <img src={assets.basket_icon} alt="Basket Icon" />
+          <div className="dot"></div>
         </div>
-        <button>Sign in
-        </button>
+        <button>Sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
