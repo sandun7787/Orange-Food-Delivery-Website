775d97a82830f6e2c0b871e2b5cf96bb312159c3
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-24 08:25:04 +0530

    update css

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploerMenu from '../../Components/ExploreMenu/ExploerMenu'
 
 const Home = () => {
+
+  const [category,setCategory]= useState("All")
   return (
     <div>
       <Header/>
-      <ExploerMenu/>
+      <ExploerMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
