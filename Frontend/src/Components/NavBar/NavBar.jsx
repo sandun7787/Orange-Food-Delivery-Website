5431a42c6188f42a9a09137fc503576acc494c3e
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-26 10:19:18 +0530

    add-new-popup

--- Frontend/src/Components/NavBar/NavBar.jsx
+++ Frontend/src/Components/NavBar/NavBar.jsx
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
 import './NavBar.css';
 import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const [menu, setMenu] = useState("home");
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="Logo" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
-        <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
-        <li onClick={() => setMenu("mobile app")} className={menu === "mobile app" ? "active" : ""}>Mobile-App</li>
-        <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact-us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          <Link to="/">Home</Link>
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          <a href="#explore-menu">Menu</a>
+        </li>
+        <li
+          onClick={() => setMenu("mobile app")}
+          className={menu === "mobile app" ? "active" : ""}
+        >
+          <a href="#app-download">Mobile-App</a>
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          <a href="#footer">Contact Us</a>
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="Search Icon" />
@@ -19,7 +41,7 @@ const NavBar = () => {
           <img src={assets.basket_icon} alt="Basket Icon" />
           <div className="dot"></div>
         </div>
-        <button>Sign in</button>
+        <button type="button">Sign in</button>
       </div>
     </div>
   );
