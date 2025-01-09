037b4a63483917165876b55abcdef7d4af2d2632
sandun7787 <sandunwarnasooreya345@gmail.com>
2025-01-09 10:43:51 +0530

    add-the-page

--- Admin/src/components/sidebar/sidebar.jsx
+++ Admin/src/components/sidebar/sidebar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './sidebar.css'
+import { assets } from '../../assets/assets'
 
 const sidebar = () => {
   return (
-    <div>
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Item</p>
+
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Item</p>
+
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Order</p>
+
+        </div>
+      </div>
       
     </div>
   )
